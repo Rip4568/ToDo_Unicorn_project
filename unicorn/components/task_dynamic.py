1890c5b2e51614b272abf17026257b33985e5582
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-10-09 16:47:15 -0300

    fazer alterações no componenete task-dynamic.html

--- unicorn/components/task_dynamic.py
+++ unicorn/components/task_dynamic.py
@@ -3,14 +3,18 @@ from django_unicorn.components import UnicornView, QuerySetType
 from Task_app.models import Task
 
 class TaskDynamicView(UnicornView):
-    tasks: QuerySetType[Task] = Task.objects.none()
+    tasks: QuerySetType[Task] = Task.objects.all()
     task: str = ""
     
+    def atualizar_tasks(self):
+        self.tasks = Task.objects.all().reverse()
+
     def salvar_task(self):#metodo para salvar, necessario chamar no html
         if self.is_valid():#se o metodo de input atual (self) for valido
             Task.objects.create(titulo=self.task)#criação e salvamento da nova task
-            self.tasks = Task.objects.all() #adicionar a lista para renderizar
-            self.tasks.reverse()#sem esse metodo a lista fica desorganizada entre o for do index e o componente
+            """ self.tasks = Task.objects.all() #adicionar a lista para renderizar
+            self.tasks.reverse()#sem esse metodo a lista fica desorganizada entre o for do index e o componente """
+            self.atualizar_tasks()
             self.task = "" #limpar o input
     
     def deletar_todas_as_tasks(self):
@@ -19,4 +23,5 @@ class TaskDynamicView(UnicornView):
         
     def deletar_task(self,task_id):
         Task.objects.get(id=task_id).delete()
-        self.tasks = Task.objects.all()
+        self.atualizar_tasks()
+        """ self.tasks = Task.objects.all() """
