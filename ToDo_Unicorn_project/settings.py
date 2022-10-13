d9a461bf210c59f043e599c78707a6c79378b3cf
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-10-13 08:59:02 -0300

    pacote pip na raiz do projeto

--- ToDo_Unicorn_project/settings.py
+++ ToDo_Unicorn_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG",default=False)) == "F"
+DEBUG = str(os.getenv("DEBUG",default=False)) == "True"
 
 
 ALLOWED_HOSTS = ['*']
